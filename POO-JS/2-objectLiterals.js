69d14db7b8085264cb44dd0d78bd3fb13181c8b9
Felipe Angelo <fangeloferreira2013@gmail.com>
2020-07-08 20:48:41 -0300

    08/07/2020-20:47-Studing POO 'finished'
    Studing POO 'finished'

--- POO-JS/2-objectLiterals.js
+++ POO-JS/2-objectLiterals.js
@@ -1,3 +1,66 @@
+//ESTE CÓDIGO TEM UM FIM DIDÁTICO E DE CONSULTA, ENTÃO NÃO SE IMPORTE COM TANTOS COMENTÁRIOS.
+
+//COMO AS COISAS FUNCIONAM POR DE BAIXO DOS PANOS-HERANÇA DE PROPRIEDADES E MÉTODOS.ENCADEAMENTO DE PROTOTYPES
+
+/*ISTO É UMA CLASSE EM SUA FORMA 'PURA', MAS A DECLARAÇÃO DE MÉTODOS DEVE SER FEITA FORA DO 'CONSTRUCTOR' ATRAVÉS DE UM 'PROTOTYPE'
+function User(_email, _name){
+    this.email = _email;
+    this.name = _name;
+    this.online = false;
+    this.login = function(){
+        console.log(this.email, 'has logged in');
+    }
+};
+*/
+
+//ISTO É UMA CLASSE ESCITA POR MEIO DE UMA FUNÇÃO-EMULATION
+function User(_email, _name){
+    this.email = _email;
+    this.name = _name;
+    this.online = false;
+};
+
+//PARA COLOACARMOS OS MÉTODOS DA CLASSE USAMOS O 'PROTOTYPE'
+User.prototype.login = function(){
+    this.online = true;
+    console.log(this.email, 'has logged in');
+};
+
+User.prototype.logout = function(){
+    this.online = false; 
+    console.log(this.email, 'has logged out');
+};
+
+//ESTA É A CLASSE 'Admin' QUE RECEBE A HERÃNÇA DE 'User', ATRAVÉS DA CAPTURA DAS PROPRIEDADES QUE FORAM TRANSPORTADAS DA DECLARAÇÃO DE 'admin' PARA UM ARRAY CHAMADO 'args'(arguments). ESTE ARRAY FOI CRIADO A PARTIR DOS TRÊS PONTOS ANTES DELE '...args', DENTRO DA CLASSE ENTÃO NÓS CHAMAMOS A CLASSE QUE SE QUER HERDAR E COLOCAMOS O PONTO SEGUIDO DA PALAVRA 'apply'. ENTRE PARÊNTESES ENTÃO, DEFINIMOS QUE A APLICAÇÃO DEVE SER FEITA NA PRÓPRIA CLASSE PASSANDO OS VALORES DO ARRAY DEFINIDO('args'),  E ASSIM OCORRE A HERÃNÇA. PARA ACRESCENTAR OUTRA PROPRIEDADE, FAZEMOS O MESMO QUE NA CLASSE INICIAL.
+function Admin(...args){
+    User.apply(this, args);
+    this.role = 'super admin';
+};
+
+//O MÉTODO 'apply' HERDA APENAS AS PROPRIEDADES DA CLASSE ESCOLHIDA, MAS NÃO HERDA OS MÉTODOS, PARA ISSO DEVEMOS DEFINIR UM PROTOTYPE PARA A 'Admin' QUE RECEBA TODOS OS PROTOTYPES DA CLASSE DESEJADA, FAZEMOS ISSO CRIANDO UM OBJETO A PARTIR DA CLASSE ANTIGA.
+Admin.prototype = Object.create(User.prototype);
+
+//COLOCAMOS UM NOVO MÉTODO PARA A CLASSE 'Admin', QUE PERTENCE APENAS A ELE.
+Admin.prototype.deleteUser = function(u){
+    users = users.filter(user => {
+        return user.email != u.email
+    });
+};
+
+//DECLARAÇÃO DAS INSTÂNCIAS CRIADAS A PARTIR DAS CLASSES CRIADAS ANTERIORMENTE.
+let userOne = new User('rafael@gmail.com','Rafael');
+let userTwo = new User('gustavo@gmail.com','Gustavo');
+let admin = new Admin('epilef@gmail.com', 'Epilef');
+
+//OUTRAS FUNCIONALIDADES
+let users = [userOne, userTwo, admin];
+
+console.log(userOne);
+userTwo.login();
+console.log(admin);
+
+/*FORMA MAIS ATUAL E RÁPIDA DE ESCRVER CLASSES
+*/
 class User {
     constructor(_name, _email){
         this.name = _name;
@@ -37,31 +100,10 @@ userOne.login().updateScore().updateScore().logout();
 
 admin.deleteUser(userTwo);
 
-console.log(users);
+console.log(users);/*
+*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//userOne.login();
-//userTwo.logout();
-
-/*
-console.log(userOne.name);
-console.log(userTwo.name);
-
+/* FORMA ANTIGA E REPETITIVA(HARD CODING-REESCREVENDO VÁRIAS VEZES A MESMA COISA)
 
 let userOne = {
     email: 'fangeloferreira2013@gmail.com',
@@ -73,4 +115,10 @@ let userOne = {
         console.log(this.email, 'has loogged out')
     }
 };
+
+userOne.login();
+userTwo.logout();
+
+console.log(userOne.name);
+console.log(userTwo.name);
 */
